869b93976a87a642a8a50566acb41d255e59d8db
zamoroz <moras8465132@gmail.com>
2020-03-29 15:56:45 +0700

    naming fix

--- app/src/main/java/com/example/lr4/DetailActivity.java
+++ app/src/main/java/com/example/lr4/DetailActivity.java
@@ -5,7 +5,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DateilActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
