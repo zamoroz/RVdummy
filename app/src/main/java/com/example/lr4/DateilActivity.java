a5d6bb73625372f4ae04f6776415ea9ee5ecbb06
zamoroz <moras8465132@gmail.com>
2020-03-29 15:55:30 +0700

    10.	Создайте класс DetailActivity, в котором необходимо выдать подробную информацию о выбранном элементе списка (см.рис.2).

---
+++ app/src/main/java/com/example/lr4/DateilActivity.java
@@ -0,0 +1,21 @@
+package com.example.lr4;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DateilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Bundle arg = getIntent().getExtras();
+        String details = arg.get("ARG_ITEM_DETAILS").toString();
+
+        TextView tv1 = findViewById(R.id.textView);
+        tv1.setText(details);
+    }
+}
