2b08129b8764fa591daa8237a6a10bfd7b7b0888
zamoroz <moras8465132@gmail.com>
2020-03-29 15:54:04 +0700

    9.	В метод onCreate(Bundle savedInstanceState) добавьте создание RecyclerView и установите для него обязательно LayoutManager и адаптер

--- app/src/main/java/com/example/lr4/MainActivity.java
+++ app/src/main/java/com/example/lr4/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.lr4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DummyContent content = new DummyContent();
+    private RViewAdapter rViewAdapter = new RViewAdapter(this, content.ITEMS);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rView = findViewById(R.id.list);
+        rView.setLayoutManager(new LinearLayoutManager(this));
+        rView.setAdapter(rViewAdapter);
     }
 }
